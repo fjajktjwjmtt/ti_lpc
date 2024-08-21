0c7f326f5a4e71b3e15e37789263e980e9c41e2f
Justin Newberry <jnewberry0502@gmail.com>
2024-08-20 21:03:23 -0400

    fixes

--- gc_rtaudio.h
+++ gc_rtaudio.h
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <fcntl.h>
 #include <time.h>
